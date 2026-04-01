676ce752dfb75803526b20ae7fd0824a05a754db
ParvaPatel <parvapatel2571@gmail.com>
2026-04-01 01:24:01 -0400

    Added custom transform

--- part-1/utils.py
+++ part-1/utils.py
@@ -33,6 +33,25 @@ def example_transform(example):
 # something called synsets (which stands for synonymous words) and for each of them, lemmas() should give you a possible synonym word.
 # You can randomly select each word with some fixed probability to replace by a synonym.
 
+def get_synonym(word):
+    synsets = wordnet.synsets(word)
+    if len(synsets) > 0:
+        lemmas = synsets[0].lemmas()
+        if len(lemmas) > 0:
+            synonym = lemmas[0].name().replace("_", " ")
+            if synonym.lower() != word.lower():
+                return synonym
+    return word
+
+
+def introduce_typo(word):
+    if len(word) <= 3:
+        return word
+    i = random.randint(0, len(word) - 2)
+    word = list(word)
+    word[i], word[i + 1] = word[i + 1], word[i]
+    return "".join(word)
+
 
 def custom_transform(example):
     ################################
@@ -44,8 +63,32 @@ def custom_transform(example):
 
     # You should update example["text"] using your transformation
 
-    raise NotImplementedError
+    # raise NotImplementedError
+    
+    text = example["text"]
 
+    # Tokenize properly (better than split)
+    words = word_tokenize(text)
+
+    new_words = []
+    for word in words:
+        r = random.random()
+
+        # 25% synonym replacement
+        if r < 0.25:
+            new_words.append(get_synonym(word))
+
+        # next 15% typo
+        elif r < 0.40:
+            new_words.append(introduce_typo(word))
+
+        else:
+            new_words.append(word)
+
+    # Detokenize back to sentence
+    detokenizer = TreebankWordDetokenizer()
+    example["text"] = detokenizer.detokenize(new_words)
+    
     ##### YOUR CODE ENDS HERE ######
 
     return example
